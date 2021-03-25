40fbd9ede97ab4534afe2d0d5d294801f65ac37b
Jacob Smith <jacob.regsmith@gmail.com>
2021-03-25 12:55:38 -0400

    Adding Initial Files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function(){
+	"use strict";	
+	console.log("fired");	
+
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burgerCon");
+
+	function hamburgerMenu(){
+		burgerCon.classList.toggle ("slideToggle");
+		button.classList.toggle("expanded");
+	}
+
+	button.addEventListener("click", hamburgerMenu);
+
+})();
